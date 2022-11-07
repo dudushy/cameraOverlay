14d7ba58c29d5e619858110c23f205acce53aec0
dudushy <eduardotalarico1337@gmail.com>
2022-11-07 18:01:52 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,15 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 
+import { CameraPreview  } from '@awesome-cordova-plugins/camera-preview/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    CameraPreview
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
