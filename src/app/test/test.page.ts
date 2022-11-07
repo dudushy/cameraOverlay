42b10adf7a831161ae56674bf4f740b5211ed10c
dudushy <eduardotalarico1337@gmail.com>
2022-11-07 18:01:55 -0300

    Update test.page.ts

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -7,6 +7,8 @@ import { AppComponent } from '../app.component';
 
 import { StorageService } from 'src/app/.services/storage.service';
 
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions } from '@awesome-cordova-plugins/camera-preview/ngx';
+
 @Component({
   selector: 'app-test',
   templateUrl: './test.page.html',
@@ -16,11 +18,31 @@ export class TestPage implements OnInit {
   title = 'TestPage';
   theme: any = 'dark';
 
+  cameraPreviewOpts: CameraPreviewOptions = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: 'rear',
+    tapPhoto: true,
+    previewDrag: true,
+    toBack: true,
+    alpha: 1,
+    storeToFile: false,
+  };
+  pictureOpts: CameraPreviewPictureOptions = {
+    width: 1280,
+    height: 1280,
+    quality: 85
+  };
+  picture: any = null;
+
   constructor(
     public app: AppComponent,
     public platform: Platform,
     private cdr: ChangeDetectorRef,
-    public storage: StorageService
+    public storage: StorageService,
+    private cameraPreview: CameraPreview
   ) {
     console.log(`[${this.title}#constructor]`);
   }
@@ -34,6 +56,8 @@ export class TestPage implements OnInit {
       console.log(`[${this.title}#ionViewDidEnter] platform.ready`, readySource);
 
       this.theme = this.storage.get('theme', this.title) == null ? 'dark' : this.storage.get('theme', this.title);
+
+      this.startCamera();
     });
   }
 
@@ -49,5 +73,56 @@ export class TestPage implements OnInit {
 
   redirectTo(url: string) {
     this.app.redirectTo(url, this.title);
+  }
+
+  startCamera() {
+    this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
+      (res) => {
+        console.log(`[${this.title}#startCamera] response`, res);
+      },
+      (err) => {
+        console.log(`[${this.title}#startCamera] error`, err);
+      });
+  }
+
+  takePicture() {
+    this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+
+      console.log(`[${this.title}#startCamera] picture`, this.picture);
+    }, (err) => {
+      console.log(`[${this.title}#startCamera] error`, err);
+      this.picture = 'assets/img/test.jpg';
+
+      console.log(`[${this.title}#startCamera] picture`, this.picture);
+    });
+  }
+
+  takeSnapshot() {
+    this.cameraPreview.takeSnapshot(this.pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+
+      console.log(`[${this.title}#takeSnapshot] picture`, this.picture);
+    }, (err) => {
+      console.log(`[${this.title}#takeSnapshot] error`, err);
+      this.picture = 'assets/img/test.jpg';
+
+      console.log(`[${this.title}#takeSnapshot] picture`, this.picture);
+    });
+  }
+
+  switchCamera() {
+    console.log(`[${this.title}#switchCamera]`);
+    this.cameraPreview.switchCamera();
+  }
+
+  setColorEffect(effect: any) {
+    console.log(`[${this.title}#setColorEffect] effect`, effect);
+    this.cameraPreview.setColorEffect(effect);
+  }
+
+  stopCamera() {
+    console.log(`[${this.title}#stopCamera]`);
+    this.cameraPreview.stopCamera();
   }
 }
